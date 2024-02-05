c13f5bede5abe704af595adc35fbf0fc9bd32232
Muhibbullah Ansary <muhibbullah611@gmail.com>
2024-02-06 03:46:15 +0600

    code refactor, and multi draggable element feature added

--- index.js
+++ index.js
@@ -1,153 +1,184 @@
 import { ref } from 'vue'
-const config = ref({
-    element: null,
-    binding: null,
-    vNode: null,
-    isMouseDown: false,
-    isMouseMove: false,
-    position: {
-        x: 0,
-        y: 0
-    },
-    calculateThePosition: () => {}
-})
+
 const useDraggable = () => {
-    const setElementPosition = (element, x, y) => {
-        element.style.top = `${y}px`
-        element.style.left = `${x}px`
-    }
-    const vDraggable = {
-        mounted(element, binding, vNode) {
-            config.value.element = element
-            config.value.binding = binding,
-            config.value.vNode = vNode
-
-            const draggable = config.value.binding.value?.draggable || true
-            const scrollableWrapper = document.querySelector(`${config.value.binding.value?.scrollableParentElement}`) || document.body
-            const boundaryElement = document.querySelector(`${config.value.binding.value?.boundaryElement}`) || document.body
-            const parentStyleInformation = boundaryElement.getBoundingClientRect()
-            const boundaryOffset = config.value.binding.value?.boundaryOffset
-            const boundary = config.value.binding.value?.boundary
-            let boundaryOffsetX = boundary 
-                                    ? (
-                                        boundaryOffset?.x || (boundaryOffset && typeof boundaryOffset != Object) 
-                                        ? boundaryOffset
-                                        : 0
-                                    ) 
-                                    : 0
-            let boundaryOffsetY = boundary 
-                                    ? (
-                                        boundaryOffset?.y || (boundaryOffset && typeof boundaryOffset != Object) 
-                                        ? boundaryOffset
-                                        : 0
-                                    ) 
-                                    : 0
-
-            config.value.position.x = config.value.binding.value?.x || 0
-            config.value.position.y = config.value.binding.value?.y || 0
-            
-            boundaryElement.style.position = 'relative'
-            config.value.element.style.userSelect = 'none'
-            config.value.element.style.position = 'absolute'
-            let mouseDownPosition = {
-                top: 0,
-                left: 0
-            }
-            
-            // initial position for draggable element start
-            setElementPosition(
-                config.value.element, 
-                (config.value.position?.x || 0) + boundaryOffsetX, 
-                (config.value.position?.y || 0) + boundaryOffsetY
-            )
-            // initial position for draggable element end
-
-
-
-            const _getBoundary = ({position, offset, widthOrHeight}) => {
-                return position < (0 + offset) ? (0 + offset)
-                            : position > (widthOrHeight - offset)
-                            ? (widthOrHeight - offset) 
-                            : position
-            }
-
-            config.value.calculateThePosition = (event) => {
-                console.log(config.value.element.offsetTop)
-                const { x, y } = event
-                const { left, top, width, height } = parentStyleInformation
-                const { left: mDownLeft, top: mDownTop } = mouseDownPosition
-                const { scrollLeft, scrollTop } = scrollableWrapper
-                let newX = (x - left) - mDownLeft + scrollLeft
-                let newY = (y - top) - mDownTop + scrollTop
-                
-                // setting boundary for x and y
-                if (boundary) 
-                {
-                    const parentWidth = width - element.offsetWidth
-                    const parentHeight = height - element.offsetHeight
-
-                    newX = _getBoundary({
-                        position: newX,
-                        offset: boundaryOffsetX,
-                        widthOrHeight: parentWidth
-                    })
-
-                    newY = _getBoundary({
-                        position: newY,
-                        offset: boundaryOffsetY,
-                        widthOrHeight: parentHeight
-                    })
-                }
-              
-                config.value.position.x = newX
-                config.value.position.y = newY
-                setElementPosition(element, newX, newY)
-            }
-              
-            config.value.element.onmousedown = function(event) {
-                if(!draggable) return
-                const targetInformation = event.target.getBoundingClientRect()
-                config.value.isMouseDown = true
-                mouseDownPosition.top = event.y - targetInformation.y
-                mouseDownPosition.left = event.x - targetInformation.x
-                if(config.value.binding.value?.onDragStart){
-                    config.value.binding.value.onDragStart(config.value.position)
-                }
-            }
-            document.addEventListener('mousemove', _handleMouseMove)
-
-            document.addEventListener('mouseup', function() {
-                if(!config.value.isMouseDown) return
-                config.value.isMouseDown = false
-                
-                if(config.value.isMouseMove){
-                    if(config.value.binding.value?.afterDragEnd){
-                        config.value.binding.value.afterDragEnd(config.value.position)
-                    }
-                }
-
-            })
-            
-        },
-        unmounted() {
-            window.removeEventListener('mousemove', _handleMouseMove)
-        }
-    }
-        
-    const _handleMouseMove = function(event) {
-        if(config.value.isMouseDown) {
-            config.value.isMouseMove = true
-            config.value.calculateThePosition(event)
-            if(config.value.binding.value?.onDragging){
-                config.value.binding.value.onDragging(config.value.position)
-            }
-        }
-    }
-
+  const createConfig = () => {
     return {
-        vDraggable,
-        position: config.value.position
+      elementInfo: {
+        boundaryElement: null,
+        scrollableWrapper: null,
+        boundaryElementStyleInformation: null,
+        boundaryOffset: null,
+      },
+      draggable: true,
+      element: null,
+      binding: null,
+      vNode: null,
+      isMouseDown: false,
+      isMouseMove: false,
+      mouseDownPosition: {
+        top: 0,
+        left: 0,
+      },
+      position: {
+        x: 0,
+        y: 0,
+      },
+      boundaryOffsetX: 0,
+      boundaryOffsetY: 0,
     }
+  }
+
+  const setElementPosition = (element, x, y) => {
+    element.style.top = `${y}px`
+    element.style.left = `${x}px`
+  }
+
+  const vDraggable = {
+    mounted(element, binding, vNode) {
+      const config = createConfig()
+
+      config.element = element
+      config.binding = binding
+      config.vNode = vNode
+
+      config.draggable =
+        config.binding.value?.draggable || true
+      config.elementInfo.scrollableWrapper =
+        document.querySelector(
+          `${config.binding.value?.scrollableParentElement}`
+        ) || document.body
+      config.elementInfo.boundaryElement =
+        document.querySelector(`${config.binding.value?.boundaryElement}`) ||
+        document.body
+      config.elementInfo.boundaryElementStyleInformation =
+        config.elementInfo.boundaryElement.getBoundingClientRect()
+      config.elementInfo.boundaryOffset =
+        config.binding.value?.boundaryOffset
+      config.elementInfo.boundary =
+        config.binding.value?.boundary
+
+      config.boundaryOffsetX = config.elementInfo.boundary
+        ? typeof config.elementInfo.boundaryOffset === 'object'
+          ? config.elementInfo.boundaryOffset?.x
+          : config.elementInfo.boundaryOffset
+        : 0
+      config.boundaryOffsetY = config.elementInfo.boundary
+        ? typeof config.elementInfo.boundaryOffset === 'object'
+          ? config.elementInfo.boundaryOffset?.y
+          : config.elementInfo.boundaryOffset
+        : 0
+
+      config.position.x = config.binding.value?.x || 0
+      config.position.y = config.binding.value?.y || 0
+
+      config.elementInfo.boundaryElement.style.position = 'relative'
+      config.element.style.userSelect = 'none'
+      config.element.style.position = 'absolute'
+
+      // initial position for draggable element start
+      setElementPosition(
+        config.element,
+        (config.position?.x || 0) + config.boundaryOffsetX,
+        (config.position?.y || 0) + config.boundaryOffsetY
+      )
+      // initial position for draggable element end
+
+      config.element.addEventListener('mousedown', (event) => _dragStart(event, config))
+      config.element.addEventListener('touchstart', (event) => _dragStart(event, config))
+
+      document.addEventListener('mousemove', (event) => _handleMouseMove(event, config))
+      document.addEventListener('touchmove', (event) => _handleMouseMove(event, config))
+
+      document.addEventListener('mouseup', () => _handleMouseUp(config))
+      document.addEventListener('touchend', () => _handleMouseUp(config))
+    },
+    unmounted(config) {
+      config.element.removeEventListener('mousedown', _dragStart)
+      window.removeEventListener('mousemove', _handleMouseMove, {passive: false})
+    },
+  }
+
+  const _getBoundary = ({ position, offset, widthOrHeight }) => {
+    return position < 0 + offset
+      ? 0 + offset
+      : position > widthOrHeight - offset
+      ? widthOrHeight - offset
+      : position
+  }
+
+  const _calculateThePosition = (event, config) => {
+    const { clientX: x, clientY: y } =
+      event.targetTouches && event.targetTouches[0] || event
+    const { left, top, width, height } =
+      config.elementInfo.boundaryElementStyleInformation
+    const { left: mDownLeft, top: mDownTop } = config.mouseDownPosition
+    const { scrollLeft, scrollTop } = config.elementInfo.scrollableWrapper
+    let newX = x - left - mDownLeft + scrollLeft
+    let newY = y - top - mDownTop + scrollTop
+
+    // setting boundary for x and y
+    if (config.elementInfo.boundary) {
+      const parentWidth = width - config.element.offsetWidth
+      const parentHeight = height - config.element.offsetHeight
+
+      newX = _getBoundary({
+        position: newX,
+        offset: config.boundaryOffsetX,
+        widthOrHeight: parentWidth,
+      })
+
+      newY = _getBoundary({
+        position: newY,
+        offset: config.boundaryOffsetY,
+        widthOrHeight: parentHeight,
+      })
+    }
+
+    config.position.x = newX
+    config.position.y = newY
+
+    setElementPosition(config.element, newX, newY)
+  }
+
+  const _dragStart = (event, config) => {
+    if (!config.draggable) return
+    const targetInformation = event.target.getBoundingClientRect()
+    config.isMouseDown = true
+
+    const { clientX, clientY } =
+      event.targetTouches && event.targetTouches[0] || event
+    config.mouseDownPosition.top = clientY - targetInformation.y
+    config.mouseDownPosition.left = clientX - targetInformation.x
+    if (config.binding.value?.onDragStart) {
+      config.binding.value.onDragStart(config.position)
+    }
+  }
+
+  const _handleMouseMove = (event, config) => {
+    if (config.isMouseDown) {
+      config.isMouseMove = true
+      _calculateThePosition(event, config)
+      if (config.binding.value?.onDragging) {
+        config.binding.value.onDragging(config.position)
+      }
+    }
+  }
+
+  const _handleMouseUp = (config) => {
+    if (!config.isMouseDown) return
+    config.isMouseDown = false
+
+    if (config.isMouseMove) {
+      if (config.binding.value?.afterDragEnd) {
+        config.binding.value.afterDragEnd(config.position)
+      }
+    }
+  }
+
+  return {
+    vDraggable,
+    position: ref({ x: 0, y: 0 }),
+  }
 }
 
 export default useDraggable
